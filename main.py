7c7a77e00b4db144ede1e093aefaeca7a8d4f2e0
Daria Zavadska <112952295+DashaZava@users.noreply.github.com>
2023-09-24 19:39:47 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,72 +1,77 @@
-from datetime import date
-import re
-
+from datetime import datetime, timedelta
 
 class Field:
-    def __init__(self, value: Any):
-        self.value = value
+    def __init__(self, value=None):
+        self.__value = value
 
-    @property
-    def value(self):
-        return self._value
+    def __get__(self, instance, owner):
+        return self.__value
 
-    @value.setter
-    def value(self, value: Any):
-        self._value = value
+    def __set__(self, instance, value):
+        self.validate(value)
+        self.__value = value
 
+    def validate(self, value):
+        pass
 
 class Phone(Field):
-    def __init__(self, number: str):
-        super().__init__(number)
-
-    def validate(self) -> bool:
-        return re.match(r"^\+380\d{9}$", self.number) is not None
-
+    def validate(self, value):
+        if value is not None and not value.isdigit():
+            raise ValueError("Phone number must contain only digits")
 
 class Birthday(Field):
-    def __init__(self, year: int, month: int, day: int):
-        super().__init__(Birthday(year, month, day))
-
-    def validate(self) -> bool:
-        return 1900 <= self.year <= 2099 and 1 <= self.month <= 12 and 1 <= self.day <= 31
-
+    def validate(self, value):
+        if value is not None:
+            try:
+                datetime.strptime(value, "%Y-%m-%d")
+            except ValueError:
+                raise ValueError("Invalid date format. Use YYYY-MM-DD")
 
 class Record:
-    def __init__(self, name: str, email: str, phone: Phone, birthday: Birthday = None):
+    def __init__(self, name, phone, birthday=None):
         self.name = name
-        self.email = email
         self.phone = phone
         self.birthday = birthday
 
-    def days_to_birthday(self) -> int:
-        if self.birthday is None:
-            return None
-        else:
-            today = date.today()
-            birthday = self.birthday.date()
-            return (birthday - today).days
-
+    def days_to_birthday(self):
+        if self.birthday:
+            birth_date = datetime.strptime(self.birthday, "%Y-%m-%d").date()
+            today = datetime.now().date()
+            next_birthday = birth_date.replace(year=today.year)
+            if today > next_birthday:
+                next_birthday = next_birthday.replace(year=today.year + 1)
+            days_remaining = (next_birthday - today).days
+            return days_remaining
+        return None
 
 class AddressBook:
     def __init__(self):
         self.records = []
 
-    def add(self, record: Record):
+    def add_record(self, record):
         self.records.append(record)
 
-    def iterator(self, n: int) -> Iterator[Record]:
-        for i in range(0, len(self.records), n):
-            yield self.records[i : i + n]
+    def remove_record(self, record):
+        self.records.remove(record)
 
+    def iterator(self, batch_size):
+        for i in range(0, len(self.records), batch_size):
+            yield self.records[i:i + batch_size]
 
-# Додавання запису з днем народження
-addressbook = AddressBook()
-addressbook.add(Record("Іван Іванович", "ivanov@example.com", Phone("+380999999999"), Birthday(2000, 12, 25)))
+# Example usage:
+if __name__ == "__main__":
+    address_book = AddressBook()
 
-# Виведення кількості днів до наступного дня народження
-print(addressbook[0].days_to_birthday())  # 191
+    record1 = Record("John Doe", "1234567890", "2000-05-15")
+    record2 = Record("Alice Smith", "9876543210")
+    record3 = Record("Bob Johnson", "5551234567", "1995-12-25")
 
-# Додавання пагінації
-for page in addressbook.iterator(5):
-    for record in page:
-        print(record.name)
+    address_book.add_record(record1)
+    address_book.add_record(record2)
+    address_book.add_record(record3)
+
+    for batch in address_book.iterator(2):
+        for record in batch:
+            print(f"Name: {record.name}, Phone: {record.phone}")
+            if record.birthday:
+                print(f"Days to Birthday: {record.days_to_birthday()}")
